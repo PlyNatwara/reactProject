aeeac372c0cc5994982f508a5e02854cf1588332
PlyNatwara <ar.natwara_st@tni.ac.th>
2022-02-09 10:54:35 +0700

    0902 Part I

---
+++ 09022022 Part I/App.js
@@ -0,0 +1,74 @@
+import {} from 'react-native';
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import DetailScreen from './screens/DetailScreen';
+import MenuScreen from './screens/MenuScreen';
+import AboutScreen from './screens/AboutScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#b19cd9'},
+        headerTintColor: '#FFFF',
+        headerTitleStyle: {fontWeight: 'bold'},
+      }}>
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{title: 'HomePage'}}
+      />
+        <Stack.Screen
+        name="AboutScreen"
+        component={AboutScreen}
+        options={{title: 'AboutUs'}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+function ProductStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#b19cd9'},
+        headerTintColor: '#FFFF',
+        headerTitleStyle: {fontWeight: 'bold'},
+      }}>
+      <Stack.Screen
+        name="ProductScreen"
+        component={ProductScreen}
+        options={{title: 'Product'}}
+      />
+        <Stack.Screen
+        name="DetailScreen"
+        component={DetailScreen}
+        options={{title: 'Detials'}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="HomeScreen"
+        drawerPosition="left"
+        drawerContent = {(props)=> <MenuScreen{...props}/>}
+      >
+        <Drawer.Screen name='HomeStack' component ={HomeStack}/>
+        <Drawer.Screen name='ProductStack' component ={ProductStack}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
